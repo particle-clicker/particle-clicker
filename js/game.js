ad9781989be90f69edb77bb7dad476d531606342
Kevin Dungs <kevin@dun.gs>
2014-08-02 09:55:55 +0200

    Development.

---
+++ js/game.js
@@ -0,0 +1,14 @@
+'use strict';
+(function () {
+
+
+
+  var app = angular.module('particleClicker', []);
+})();
+
+var discoveries = [
+  {
+    name: 'W-Boson',
+    description: 'Some nice description.'
+  }
+];

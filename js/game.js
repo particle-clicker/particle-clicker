1ef115e16759318027e7dbf7b81af281e6bae67d
Kevin Dungs <kevin@dun.gs>
2014-08-02 10:26:28 +0200

    Implement basic JSON file loading functionality.

--- js/game.js
+++ js/game.js
@@ -1,14 +1,25 @@
 'use strict';
 (function () {
+  var loadJsonFile = function(filename) {
+    var res;
+    $.ajax({
+            async: false,
+            url : filename,
+            success : function(data) {
+              res = data;
+            }
+    });
+    return res;
+  }
 
 
+  var discoveries = loadJsonFile('json/discoveries.json');
+  var workers = loadJsonFile('json/workers.json');
+
+  console.log(discoveries);
+  console.log(workers);
 
   var app = angular.module('particleClicker', []);
 })();
 
-var discoveries = [
-  {
-    name: 'W-Boson',
-    description: 'Some nice description.'
-  }
-];
+

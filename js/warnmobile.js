c07fbd43e9615a976e1290f96fac881ec2007604
Kevin Dungs <kevin@dun.gs>
2014-08-06 18:04:07 +0200

    Get warning on index.html to work.

--- js/warnmobile.js
+++ js/warnmobile.js
@@ -1,8 +1,7 @@
-$(function () {
+'use strict';
+
+(function() {
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    alert('lol');
     $('#MobileWarning').modal('show');
-  } else {
-    Console.log(navigator.userAgent);
   }
-});
+})();

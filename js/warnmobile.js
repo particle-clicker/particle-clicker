99624405ee3ca3d88c1a5ac0f53532199d4ccbfc
Kevin Dungs <kevin@dun.gs>
2014-11-09 16:12:59 +0100

    Fix use strict. Also add matcher for Firefox OS(?).

--- js/warnmobile.js
+++ js/warnmobile.js
@@ -1,7 +1,6 @@
-'use strict';
-
 (function() {
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  'use strict';
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile/i.test(navigator.userAgent)) {
     $('#MobileWarning').modal('show');
   }
 })();

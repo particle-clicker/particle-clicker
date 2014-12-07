a4465abb49b2d9b98ed66c6dc58ce91926196811
Tadej Novak <tadej@tano.si>
2014-12-07 16:18:49 +0100

    Remove mobile detection

--- js/warnmobile.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  'use strict';
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile/i.test(navigator.userAgent)) {
-    $('#MobileWarning').modal('show');
-  }
-})();
